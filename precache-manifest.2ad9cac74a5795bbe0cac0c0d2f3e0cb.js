ab874c820a4cbc58820707ae32c6cd6ec6f76646
Daniela Llano <daniela.llano1@correo.icesi.edu.co>
2020-10-22 15:20:21 -0500

    Updates

---
+++ precache-manifest.2ad9cac74a5795bbe0cac0c0d2f3e0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f20589e5e7be37cd5ff6927679d5d0",
+    "url": "/TallerReact-WebAvanzada/index.html"
+  },
+  {
+    "revision": "89af834d3c8cc7e56979",
+    "url": "/TallerReact-WebAvanzada/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "92069389d2982004075b",
+    "url": "/TallerReact-WebAvanzada/static/js/2.5f9c9eeb.chunk.js"
+  },
+  {
+    "revision": "573877c726894718fb13231e54d73766",
+    "url": "/TallerReact-WebAvanzada/static/js/2.5f9c9eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89af834d3c8cc7e56979",
+    "url": "/TallerReact-WebAvanzada/static/js/main.be29f3c9.chunk.js"
+  },
+  {
+    "revision": "0d85e9ea660ee4078ee8",
+    "url": "/TallerReact-WebAvanzada/static/js/runtime-main.9c1f2752.js"
+  }
+]);
